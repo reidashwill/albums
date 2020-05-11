6b926cc21f6ca15336aab66899f04e5b016d17ff
Reid Ashwill <reidashwill@gmail.com>
2020-05-11 13:01:49 -0700

    get all tests working

--- lib/albums.rb
+++ lib/albums.rb
@@ -1,10 +1,47 @@
 require ('pry')
 
 class Album
+  attr_reader :id, :name
   attr_accessor :name
+  @@albums = {}
+  @@total_rows = 0
 
-  def initialize(name)
+  # def ==(other_album)
+  #   self.name.eql?(other_album_name) && self.artist.eql?(other_album.artist) && self.year.eql?(other_album.year)
+  # end
+  
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1
+  end
+  
+  def self.all()
+    @@albums.values()
+  end
+  
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
   end  
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+ 
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0 
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+  
+  def update(name)
+    @name = name
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
 
 end
